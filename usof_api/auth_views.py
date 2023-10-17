1f8680db4ce44e9d15167f98fc617313a94390a4
kreker783 <kreker783@gmail.com>
2023-10-17 23:41:31 +0200

    Implement register and login api

--- usof_api/auth_views.py
+++ usof_api/auth_views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework import views
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import redirect
 from rest_framework.response import Response
 from .models import Post, Category, Comment, Like, User
@@ -26,26 +26,25 @@ def register(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class LoginView(views.APIView):
+@api_view(['POST'])
+def loginView(request):
+    serialize = LoginSerializer(data=request.data)
 
-    def post(self, request):
-        # serializer = LoginSerializer(data=request.data,
-        #                              context={'request': request})
-        # if serializer.is_valid():
-        #     user = serializer.validated_data['user']
-        #     login(request, user)
-        #     return Response(None, status=status.HTTP_202_ACCEPTED)
-        serialize = LoginSerializer(data=request.data)
+    if serialize.is_valid():
+        user = authenticate(login=request.data.get('login'),
+                            email=request.data.get('email'),
+                            password=request.data.get('password'))
 
-        if serialize.is_valid():
-            user = authenticate(login=request.data.get('login'),
-                                email=request.data.get('email'),
-                                password=request.data.get('password'))
+        if user is not None:
+            login(request, user)
+            return redirect('users')
+        else:
+            return Response('User does not exist', status=status.HTTP_400_BAD_REQUEST)
 
-            if user is not None:
-                login(request, user)
-                return redirect('/usof/api/users/')
-            else:
-                return Response('User does not exist', status=status.HTTP_400_BAD_REQUEST)
+    return Response(serialize.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        return Response(serialize.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def logoutView(request):
+    logout(request)
+    return redirect('users')
--- usof_api/urls.py
+++ usof_api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from .views import PostApiView
-from .auth_views import register, LoginView
+from .auth_views import register, loginView
 from .user_views import UsersView
 
 urlpatterns = [
     path('post/', PostApiView.as_view()),
     path('auth/register/', register),
     path('users/<str:username>/', UsersView.as_view()),
-    path('users/', UsersView.as_view()),
-    path('auth/login/', LoginView.as_view())
+    path('users/', UsersView.as_view(), name='users'),
+    path('auth/login/', loginView)
 ]
